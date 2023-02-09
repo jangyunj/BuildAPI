0809b8d7599359b7268d69cdf37d87c36c685b34
Yun Jang <jang.yunj7@gmail.com>
2023-02-08 19:02:04 -0500

    added source

--- index.js
+++ index.js
@@ -4,19 +4,44 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const app = express();
+const newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+  },
+];
+const articles = [];
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      articles.push({
+        title,
+        url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
 
 app.get("/", (req, res) => {
   res.json("Welcome!");
 });
 
 app.get("/news", (req, res) => {
-  axios
-    .get("https://www.cnn.com/2023/02/08/business/disney-earnings/index.html")
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('a:contains("disney")', html).each(function () {})
-    });
+  res.json(articles);
 });
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
